176863e381af9defd009b2731c9e605b4e1eb8f0
Absolpega <absolpega@gmail.com>
2023-07-02 22:50:12 +0300

    onedark colorsheme

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -2,13 +2,31 @@ return {
     { "elkowar/yuck.vim" },
 
     {
+        "navarasu/onedark.nvim",
+        opts = {
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                --floats = "transparent",
+            },
+        },
+    },
+
+    {
         "tokyonight.nvim",
         opts = {
             transparent = true,
             styles = {
-                --sidebars = "transparent",
+                sidebars = "transparent",
                 --floats = "transparent",
             },
+        },
+    },
+
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "onedark",
         },
     },
 }
